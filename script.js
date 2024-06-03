ceaa0e8dc406bb91cca1ade1b0495468af940135
jakobagger <113458342+jakobagger@users.noreply.github.com>
2024-06-03 12:53:50 +0200

    del 3 færdig

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// *******************************CONTROLLER*******************************************************************
+//#region CONTROLLER
 
 window.addEventListener("load", start);
 
@@ -11,11 +11,13 @@ function start(){
 
 function selectCell(row, col){
     writeToCell(row, col, 1);
+    displayBoard();
     console.table(model);
 }
 
+//#endregion
 
-//****************************** VIEW **********************************************************************
+//#region VIEW
 
 function makeBoardClickable(){
     document.querySelector("#board")
@@ -32,7 +34,24 @@ function boardClicked(event){
     selectCell(row, col);
 }
 
-// *************************************MODEL******************************************************************
+function displayBoard(){
+    for (let row = 0; row < 3; row++){
+        for (let col = 0; col < 3; col++){
+            
+            const value = readFromCell(row, col);
+            const cell = document.querySelector(`[data-row="${row}"][data-col="${col}"]`);
+
+            switch (value){
+                case 0: cell.textContent = " "; break;
+                case 1: cell.textContent = "X"; break;
+                case 2: cell.textContent = "O"; break;
+            }
+        }
+    }
+}
+//#endregion
+
+//#region MODEL
 
 const model = [
     [0,0,0],
@@ -47,3 +66,5 @@ function writeToCell(row, col, value){
 function readFromCell(row, col){
     return model[row][col];
 }
+
+//#endregion
